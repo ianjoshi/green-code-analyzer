9bf908dcec7fea0ebd5a7a35784c638170032960
Todor-cmd <79723091+Todor-cmd@users.noreply.github.com>
2025-03-18 20:58:46 +0100

    experiments work

--- experiments/rule_impact_experiment.py
+++ experiments/rule_impact_experiment.py
@@ -4,33 +4,34 @@ import os
 from energibridge_executor import EnergibridgeExecutor
 from pyEnergiBridge.api import EnergiBridgeRunner
 
-class RuleImpactEstimator:
+
+
+
+class RuleImpactExperiment:
     """
     A class for running controlled energy consumption measurement experiments for the rules,
     in order to estimate their impact on the energy consumption of the system.
     The experiment includes a warm-up phase, task execution, and rest periods between runs.
     """
 
-    def __init__(self, rules = None,num_runs=5, warmup_duration=300, rest_duration=60, measurement_duration=10):
+    def __init__(self, rules, num_runs=5, warmup_duration=300, rest_duration=60, measurement_duration=500):
         """
         Initializes the experiment with the necessary parameters.
 
         Parameters:
-        - rules (dict): Dictionary of rules to be tested with rule name as key and path to the rule's file directory as value.
+        - rules (dict): Dictionary of rules to be tested with rule name as key and a list of functions that execute an example of the rule as value.
         - num_runs (int): Number of times each task should be executed.
         - warmup_duration (int): Warm-up period (in seconds) before measurements.
         - rest_duration (int): Rest period (in seconds) between runs.
         - measurement_duration (int): Maximum duration of each measurement in seconds.
         """
+        self.rules = rules
         self.num_runs = num_runs
         self.warmup_duration = warmup_duration
         self.rest_duration = rest_duration
 
         self.energibridge = EnergibridgeExecutor(max_measurement_duration=measurement_duration)
-        self.energibridge_runner = EnergiBridgeRunner(max_duration=measurement_duration)
-
-        # Dictionary mapping task names to functions for experiment
-        self.tasks = {}
+        self.energibridge_runner = EnergiBridgeRunner()
         
         # Create results directory if it doesn't exist
         os.makedirs("results/rule_impact_estimates", exist_ok=True)
@@ -45,30 +46,25 @@ class RuleImpactEstimator:
         2. Warms up the CPU by running Fibonacci calculations.
         3. Runs each task multiple times in a shuffled order with rest intervals.
         """
-        self.generate_tasks()
-
-        if not self.tasks:
-            raise ValueError("No tasks have been set.")
-
         self._warn_and_prepare()
-        self._warmup_fibonacci()
+        #self._warmup_fibonacci()
         self.energibridge.start_service()
 
-        # Create a list of (task_name, task_function, run_index) tuples for shuffling
-        task_run_list = [(name, func, i + 1) for name, func in self.tasks.items() for i in range(self.num_runs)]
-        random.shuffle(task_run_list)  # Shuffle task execution order
+        rule_names = list(self.rules.keys())
+        random.shuffle(rule_names)
+        for i in range(self.num_runs):
+            for rule_name in rule_names:
+                print(f"----- Run {rule_name} -----")
+                output_file = f"results/rule_impact_estimates/{rule_name}.csv"
+                rule_functions = self.rules[rule_name]
 
-        for run_index, (task_name, task_func, run_id) in enumerate(task_run_list, 1):
-            print(f"----- Run {run_index} (Task: {task_name}, Instance: {run_id}) -----")
-            output_file = f"results/{task_name}_run_{run_id}.csv"
+                print(f"Estimating the impact of the rule {rule_name} on the energy consumption of a system.")
+                self._estimate_rule_impact(rule_functions, output_file)
 
-            # Execute the task
-            task_func(output_file=output_file)
-
-            # Rest between runs except for the last iteration
-            if run_index < len(task_run_list):
-                print(f"Resting for {self.rest_duration} seconds before the next run...")
-                time.sleep(self.rest_duration)
+                # Rest between runs except for the last iteration of last run
+                if i < self.num_runs - 1:
+                    print(f"Resting for {self.rest_duration} seconds before the next run...")
+                    time.sleep(self.rest_duration)
 
         self.energibridge.stop_service()
         print("Experiment complete.")
@@ -106,41 +102,27 @@ class RuleImpactEstimator:
             a, b = b, a + b
         return b
     
-    def regex_matching(self, engine, file_size, regex_pattern, output_file = None):
-        """
-        Perform regex matching using the specified engine, corpus file size, and regex pattern.
-        """
-        if output_file is None:
-            print("No output file provided, using default name")
-            output_file = f"results/{engine}_{file_size}_{regex_pattern}.csv"
-
-        # Retrieve the corpus file based on the file size
-        corpus = f"data/{file_size}.txt"
-
-        # Create an instance of the RegexEnginesExecutor
-        regex_engine_executor = RegexEnginesExecutor(
-            regex_engine=engine,
-            corpus=corpus,
-            pattern=regex_pattern,
-            energy_bridge_runner=self.energibridge_runner
-        )
-
-        # Set up the regex engine
-        regex_engine_executor.setUp()
-
-        # Retrieve the method name from the dictionary and call it dynamically
-        method_name = RegexEnginesExecutor.engine_methods.get(engine)
-        if method_name is None:
-            raise ValueError(f"Unknown regex engine: {engine}")
-
-        # Call the method dynamically using `getattr`
-        output = getattr(regex_engine_executor, method_name)(output_file)
-        print("Found matches:", output)
-
-        # regex_engine_executor.tearDown()
-
-        return output
-
+    def _estimate_rule_impact(self, rule_examples, output_file):
+        """Estimates the impact of the rule on the energy consumption of the system."""
+        # Shuffle order of rule examples
+        random.shuffle(rule_examples)
+        
+        # Start measuring the energy consumption
+        energibridge_runner = EnergiBridgeRunner()
+        energibridge_runner.start(output_file)
+        
+        # Execute each function containing an example of the rule
+        for rule_example in rule_examples:
+            rule_example()
+        
+        # Stop measuring the energy consumption
+        energibridge_runner.stop()
+        
+          
 if __name__ == "__main__":
-    experiment = EnergyExperiment()
+    import sys
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    from data.long_loop import inefficient_loop
+    rules = {"long_rule": [inefficient_loop]}
+    experiment = RuleImpactExperiment(rules)
     experiment.run_experiment()
