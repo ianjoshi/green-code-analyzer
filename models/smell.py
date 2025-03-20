a2b2a29c18007318bed773d7f32cdf7ea798a987
ianjoshi <55448380+ianjoshi@users.noreply.github.com>
2025-03-20 12:47:18 +0100

    Add optimization to smells and rules

--- models/smell.py
+++ models/smell.py
@@ -7,24 +7,28 @@ class Smell:
     Represents a detected energy code smell.
 
     Attributes:
-    - rule_id (str): The ID of the rule that detected the smell.
-    - rule_name (str): The name of the rule that detected the smell.
-    - message (str): A description of the detected smell.
-    - penalty (float): The penalty applied to the energy score due to the smell, which starts at 100.
-    - start_line (int): The starting line number where the smell occurs.
-    - end_line (Optional[int]): The ending line number of the smell occurrence (if applicable). If null, assume smell only covers single line.
+        - rule_id (str): The ID of the rule that detected the smell.
+        - rule_name (str): The name of the rule that detected the smell.
+        - description (str): A description of the detected smell.
+        - penalty (float): The penalty applied to the energy score due to the smell, which starts at 100.
+        - start_line (int): The starting line number where the smell occurs.
+        - end_line (Optional[int]): The ending line number of the smell occurrence (if applicable).
+          If null, assume smell only covers single line.
+        - optimization (Optional[str]): Possible solution or solutions for the energy code smell, if available.
     """
     rule_id: str
     rule_name: str
-    message: str
+    description: str
     penalty: float
     start_line: int
     end_line: Optional[int] = None
+    optimization: Optional[str] = None
 
     def __str__(self):
         """String representation."""
         line_info = f"Lines {self.start_line}-{self.end_line}" if self.end_line else f"Line {self.start_line}"
+        optimization_info = f", Optimization: {self.optimization}" if self.optimization else ""
         return (f"Rule ID: {self.rule_id}, Rule Name: {self.rule_name}, "
-                f"Message: {self.message}, Penalty: {self.penalty:.2f}, "
+                f"Description: {self.description}, Penalty: {self.penalty:.2f}"
+                f"{optimization_info}, "
                 f"Affected Line(s): {line_info}")
-
